fc9a1ea092d52c374e75dc06956f0c7a2cefb520
matthijsdentoom <m.dentoom@student.tudelft.nl>
2019-05-17 14:00:33 +0200

    Added trajectory visualization at map level, with multiple runners.

--- scripts/ros_robot_experiment.py
+++ scripts/ros_robot_experiment.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 import time
 from random import randint
-from threading import Condition, Lock
+from threading import Condition
 
 import rospy
 from examples.experiment_template import SingleExperiment
 from ma_evolution.msg import Score
-
-from tools.score_saver import ScoreSaver
-
+from ma_evolution.srv import SimScore
+from ma_evolution.srv import Trajectory
 
 class SimulationCommunicator:
     """ This class takes communication with one simulation runner."""
 
     def __init__(self, namespace, message_type, condition_lock):
         print(namespace)
+        self.namespace = namespace
         self.condition_lock = condition_lock
-        self.scores_lock = Lock()
-        self.genome_publisher = rospy.Publisher(namespace + 'genome_topic', message_type, queue_size=100)
+        self.genome_publisher = rospy.Publisher(self.namespace + 'genome_topic', message_type, queue_size=100)
         self.retrieved_scores = {}
         self.gen_hash = 0
-        rospy.Subscriber(namespace + 'score_topic', Score, self.callback)
+        rospy.Subscriber(self.namespace + 'score_topic', Score, self.callback)
 
     def reset(self):
         self.gen_hash = 0
@@ -29,6 +28,17 @@ class SimulationCommunicator:
     def publish_genome(self, enc_genome):
         self.gen_hash = enc_genome.gen_hash
         self.genome_publisher.publish(enc_genome)
+
+    def get_score(self):
+        """ This function returns the last score of the simulator this runner controls."""
+        rospy.wait_for_service(self.namespace + 'score')
+        score_service = rospy.ServiceProxy(self.namespace + 'score', SimScore)
+        return score_service()
+
+    def get_trajectory(self):
+        rospy.wait_for_service(self.namespace + 'trajectory')
+        trajectory_service = rospy.ServiceProxy(self.namespace + 'trajectory', Trajectory)
+        return trajectory_service()
 
     def callback(self, data):
         # Put the retrieved score in a list and notify (trough the condition) that a new score has arrived.
--- scripts/run_and_visualize.py
+++ scripts/run_and_visualize.py
@@ -1,25 +1,70 @@
 #!/usr/bin/env python
+import glob
+import pickle
 import sys
 from multiprocessing import Condition
 from time import sleep
 import matplotlib.pyplot as plt
 
 import rospy
-from neat.object_serializer import ObjectSerializer
 
 from message_parsing import NEATROSEncoder, SMROSEncoder
 from ros_robot_experiment import SimulationCommunicator
-from ma_evolution.srv import Trajectory
-from ma_evolution.srv import SimScore
 
 matplotlib_color_values = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+
+
+def create_visualization(genome_locations, encoder, sc, visualize_loc='scenario.png'):
+    """ This method creates a visualization and writes it back to the same folder."""
+    plt.clf()
+
+    num = 0
+    for genome_location in genome_locations:
+
+        sc.reset()
+
+        # Load and encode the genome.
+        with open(genome_location, 'rb') as handle:
+            genome = pickle.load(handle)
+
+        ros_encoded_genome = encoder.encode(genome, 0)
+
+        sc.condition_lock.acquire()
+        sc.publish_genome(ros_encoded_genome)
+
+        while not sc.condition_lock.wait(1.0):
+            if len(sc.retrieved_scores) == 1:
+                break
+
+            if rospy.is_shutdown():
+                exit(1)
+        sc.condition_lock.release()
+
+        # Get the result.
+        trajectory = sc.get_trajectory()
+        score = sc.get_score()
+        print('got a score of {0:.2f}'.format(score.score))
+
+        x_locs = [loc.x for loc in trajectory.Locations]
+        y_locs = [loc.y for loc in trajectory.Locations]
+
+        plt.plot(y_locs, x_locs, matplotlib_color_values[num % len(matplotlib_color_values)],
+                 label='gen score: {0:.2f}'.format(score.score))
+        num += 1
+
+    plt.axis('scaled')
+    plt.legend()
+    plt.xlim((0, 5))
+    plt.ylim((-1.5, 6.5))
+    plt.savefig(visualize_loc)
+
 
 if __name__ == '__main__':
 
     my_argv = rospy.myargv(argv=sys.argv)
 
     if len(my_argv) < 3:
-        print('Expected genomes encoding an the the location(s) of the genome(s) as argument.')
+        print('Expected genomes encoding an the the folder of the genome as argument.')
         exit(1)
 
     encoder = None
@@ -36,50 +81,19 @@ if __name__ == '__main__':
     # Initialises the communication.
     rospy.init_node('run_genome_from_file', anonymous=True)
     condition_lock = Condition()
-    sc = SimulationCommunicator('', encoder.get_message_type(), condition_lock)
-    sleep(1)
 
-    num = 0
-    for genome_location in my_argv[2:]:
+    # Find all the topic scores.
+    search_string = 'score_topic'
+    score_topics = [topic[0] for topic in rospy.get_published_topics() if search_string in topic[0]]
+    name_spaces = [topic.replace(search_string, '') for topic in score_topics]
+    sim_controllers = [SimulationCommunicator(ns, encoder.get_message_type(), condition_lock)
+                       for ns in name_spaces]
+    sleep(1)  # Sleep is required for initialisation
 
-        sc.reset()
+    genome_locations = glob.glob(my_argv[2] + 'winner*.pickle')
 
-        # Load and encode the genome.
-        genome = ObjectSerializer.load(genome_location)
-        ros_encoded_genome = encoder.encode(genome, 0)
+    for sc in sim_controllers:
 
-        condition_lock.acquire()
-        sc.publish_genome(ros_encoded_genome)
-
-        while not condition_lock.wait(1.0):
-            if len(sc.retrieved_scores) == 1:
-                break
-
-            if rospy.is_shutdown():
-                exit(1)
-        condition_lock.release()
-
-        # Get the result.
-        trajectory_service = rospy.ServiceProxy('trajectory', Trajectory)
-        trajectory = trajectory_service()
-
-        score_service = rospy.ServiceProxy('score', SimScore)
-        score = score_service()
-        print('got a score of {0}'.format(score))
-
-        x_locs = [loc.x for loc in trajectory.Locations]
-        y_locs = [loc.y for loc in trajectory.Locations]
-
-        plt.plot(y_locs, x_locs, matplotlib_color_values[num])
-        num += 1
-
-    plt.xlim((0, 5))
-    plt.ylim((-1.5, 6.5))
-
-    plt.show()
-
-
-
-
-
-
+        filename = 'scenario' + filter(str.isdigit, sc.namespace) + '.png'
+        print(filename)
+        create_visualization(genome_locations, encoder, sc, filename)
