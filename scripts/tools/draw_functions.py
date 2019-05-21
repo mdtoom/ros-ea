a4655406c1b1be0775b012f1aad36e761c84de56
matthijsdentoom <m.dentoom@student.tudelft.nl>
2019-05-21 15:40:57 +0200

    Removed unused main from draw functions

--- scripts/tools/draw_functions.py
+++ scripts/tools/draw_functions.py
@@ -1,6 +1,5 @@
-import pickle
-import sys
 from subprocess import CalledProcessError
+
 from neat import visualize
 
 
@@ -15,22 +14,3 @@ def draw_sm(genome, config, file_name='winner.svg'):
 def draw_nn(genome, config, file_name='winner.svg'):
     """ This function draws a neural network. """
     visualize.draw_net(config, genome, filename=file_name)
-
-
-if __name__ == '__main__':
-
-    if len(sys.argv) < 3:
-        print('Expected the type of controller and at least one controller file.')
-
-    controller_name = sys.argv[1]
-    draw_function = None
-    if controller_name == 'state-machine':
-        draw_function = draw_sm
-    elif controller_name == 'neural-network':
-        draw_function = draw_nn
-
-    for controller_file in sys.argv[2:]:
-        with open(controller_file, 'rb') as handle:
-            controller = pickle.load(handle)
-
-
