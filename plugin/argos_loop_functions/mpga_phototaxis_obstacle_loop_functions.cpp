85b54abb86d567452742d5b146bfd8a73c1e2829
matthijsdentoom <m.dentoom@student.tudelft.nl>
2019-05-28 14:36:30 +0200

    Removed unnecessary class and added parameter for fitness power.

--- plugin/argos_loop_functions/mpga_phototaxis_obstacle_loop_functions.cpp
+++ plugin/argos_loop_functions/mpga_phototaxis_obstacle_loop_functions.cpp
@@ -10,12 +10,56 @@
 
 using namespace argos;
 
-#define FITNESS_POWER 3            // This define is used to indicate the shape of the fitness function.
+#define FITNESS_POWER_DEFAULT 3            // This define is used to indicate the shape of the fitness function.
 
 const Real maxDistance = 11.0;      // Constant used to indicate the max distance from the robot to the light.
 
-CMPGAPhototaxisObstacleLoopFunctions::CMPGAPhototaxisObstacleLoopFunctions() : m_fScore(0), m_fMaxDistance(maxDistance)
+CMPGAPhototaxisObstacleLoopFunctions::CMPGAPhototaxisObstacleLoopFunctions() :
+    m_fScore(0), m_fMaxDistance(maxDistance), m_pcFootBot(NULL), m_pcRNG(NULL),
+    m_bDone(false), m_fFitnessPower(FITNESS_POWER_DEFAULT)
 { }
+
+/****************************************/
+/****************************************/
+
+CMPGAPhototaxisObstacleLoopFunctions::~CMPGAPhototaxisObstacleLoopFunctions()
+{
+    delete m_pcFootBot;
+}
+
+/****************************************/
+/****************************************/
+
+void CMPGAPhototaxisObstacleLoopFunctions::Init(TConfigurationNode& t_node)
+{
+
+    /* Create the random number generator. */
+    m_pcRNG = CRandom::CreateRNG("argos");
+
+    // Set the power attribute which is used for deciding the power of the fitness calculation.
+    GetNodeAttributeOrDefault(t_node, "fitness_power", m_fFitnessPower, m_fFitnessPower);
+
+    SetStartLocation();
+
+    // Register a reset service at the node of the simulation.
+    m_pcResetService = CArgosRosBot::nodeHandle->
+            advertiseService("reset", &CMPGAPhototaxisObstacleLoopFunctions::ResetRobot, this);
+
+    // Register the get score service of the node of the simulation.
+    m_pcScoreService = CArgosRosBot::nodeHandle->
+            advertiseService("score", &CMPGAPhototaxisObstacleLoopFunctions::GetScore, this);
+
+    m_pcTrajectoryService = CArgosRosBot::nodeHandle->
+            advertiseService("trajectory", &CMPGAPhototaxisObstacleLoopFunctions::GetTrajectory, this);
+
+    m_pcDoneService = CArgosRosBot::nodeHandle->advertiseService("done",
+            &CMPGAPhototaxisObstacleLoopFunctions::IsDone, this);
+
+    // Create the foot-bot and get a reference to its controller
+    m_pcFootBot = new CFootBotEntity("fb", "argos_ros_bot");
+    AddEntity(*m_pcFootBot);
+    Reset();
+}
 
 void CMPGAPhototaxisObstacleLoopFunctions::SetStartLocation() {
 
@@ -36,9 +80,61 @@ void CMPGAPhototaxisObstacleLoopFunctions::SetStartLocation() {
 
 void CMPGAPhototaxisObstacleLoopFunctions::Reset() {
 
-    CMPGAPhototaxisLoopFunctions::Reset();
+    /*
+    * Move robot to the initial position corresponding to the current trial
+    */
+    if(!MoveEntity(
+            m_pcFootBot->GetEmbodiedEntity(),             // move the body of the robot
+            m_vecResetLocation.Position,    // to this position
+            m_vecResetLocation.Orientation, // with this orientation
+            false                                         // this is not a check, leave the robot there
+    )) {
+        LOGERR << "Can't move robot in <" << m_vecResetLocation.Position
+               << ">, <" << m_vecResetLocation.Orientation << ">" << std::endl;
+    }
 
+    m_vLocations.clear();
+    m_bDone = false;
     m_fScore = 0;
+}
+
+bool CMPGAPhototaxisObstacleLoopFunctions::ResetRobot(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response) {
+
+    Reset();
+    return true;
+
+}
+
+/****************************************/
+/****************************************/
+
+bool CMPGAPhototaxisObstacleLoopFunctions::GetScore(ma_evolution::SimScore::Request& request,
+                                            ma_evolution::SimScore::Response& response)
+{
+    Real score = Score();
+    response.score = (float) score;
+    return true;
+}
+
+/****************************************/
+/****************************************/
+
+bool CMPGAPhototaxisObstacleLoopFunctions::IsDone(ma_evolution::Done::Request& request, ma_evolution::Done::Response& response)
+{
+    response.done = m_bDone;
+    return true;
+}
+
+
+bool CMPGAPhototaxisObstacleLoopFunctions::GetTrajectory(ma_evolution::Trajectory::Request& request,
+                                                 ma_evolution::Trajectory::Response& response)
+{
+    for(std::vector<geometry_msgs::Point>::iterator it = m_vLocations.begin(); it != m_vLocations.end(); ++it)
+    {
+        response.Locations.emplace_back(*it);
+    }
+
+    return true;
 }
 
 /****************************************/
@@ -46,7 +142,13 @@ void CMPGAPhototaxisObstacleLoopFunctions::Reset() {
 
 void CMPGAPhototaxisObstacleLoopFunctions::PostStep() {
 
-    CMPGAPhototaxisLoopFunctions::PostStep();
+    // Add the current robot location to the list of visited locations.
+    CVector3 robotPosition = m_pcFootBot->GetEmbodiedEntity().GetOriginAnchor().Position;
+    geometry_msgs::Point point;
+    point.x = robotPosition.GetX();
+    point.y = robotPosition.GetY();
+    point.z = robotPosition.GetZ();
+    m_vLocations.emplace_back(point);
 
     if (!m_bDone)
     {
@@ -55,7 +157,6 @@ void CMPGAPhototaxisObstacleLoopFunctions::PostStep() {
         {
             m_bDone = true;
         }
-
         m_fScore += CalculateStepScore();
     }
 }
@@ -84,7 +185,7 @@ Real CMPGAPhototaxisObstacleLoopFunctions::CalculateStepScore()
 Real CMPGAPhototaxisObstacleLoopFunctions::calculateFitness(Real distance)
 {
     Real normalizedDistance = 1.0 - distance / m_fMaxDistance;
-    return pow(normalizedDistance, FITNESS_POWER);
+    return pow(normalizedDistance, m_fFitnessPower);
 }
 
 
--- plugin/argos_loop_functions/mpga_phototaxis_obstacle_loop_functions.h
+++ plugin/argos_loop_functions/mpga_phototaxis_obstacle_loop_functions.h
@@ -4,37 +4,72 @@
 /* ARGoS-related headers */
 #include <argos3/core/utility/math/rng.h>
 #include <argos3/plugins/robots/foot-bot/simulator/footbot_entity.h>
-
-#include "mpga_phototaxis_loop_functions.h"
+#include <argos3/core/simulator/loop_functions.h>
+#include <argos3/core/simulator/entity/embodied_entity.h>
 
 #include "ros/ros.h"
 #include "std_srvs/Empty.h"
 #include "ma_evolution/SimScore.h"
+#include "geometry_msgs/Point.h"
+#include "ma_evolution/Trajectory.h"
+#include "ma_evolution/Done.h"
 
 /****************************************/
 /****************************************/
 
 using namespace argos;
 
-class CMPGAPhototaxisObstacleLoopFunctions : public CMPGAPhototaxisLoopFunctions
+/* The initial setup of a trial */
+struct SInitSetup {
+    CVector3 Position;
+    CQuaternion Orientation;
+};
+
+
+class CMPGAPhototaxisObstacleLoopFunctions : public CLoopFunctions
 {
 
 public:
 
     CMPGAPhototaxisObstacleLoopFunctions();
 
-    virtual ~CMPGAPhototaxisObstacleLoopFunctions() = default;
+    virtual ~CMPGAPhototaxisObstacleLoopFunctions();
 
-    virtual void Reset() override;
+    virtual void Init(TConfigurationNode& t_node);
 
-    virtual void SetStartLocation() override;
+    virtual void Reset();
 
-    virtual void PostStep() override;
+    virtual void SetStartLocation();
+
+    virtual void PostStep();
 
     /* Calculates the performance of the robot in a trial */
-    virtual Real Score() override;
+    virtual Real Score();
+
+
+    /** This function resets the robot to its original position. */
+    virtual bool ResetRobot(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response);
+
+    /** This function gets the score of the robot. */
+    virtual bool GetScore(ma_evolution::SimScore::Request& request, ma_evolution::SimScore::Response& response);
+
+    /** Returns true if the robot is done. */
+    virtual bool IsDone(ma_evolution::Done::Request& request, ma_evolution::Done::Response& response);
+
+    /** This function returns the latest trajectory of the robot until reset. */
+    virtual bool GetTrajectory(ma_evolution::Trajectory::Request& request, ma_evolution::Trajectory::Response& response);
+
 
 protected:
+
+    /** This server keeps the messages for the reset service coming. */
+    ros::ServiceServer m_pcResetService;
+    /** This server returns the current score of the simulation. */
+    ros::ServiceServer m_pcScoreService;
+    /** This returns the list of locations of the robot from the latest reset. */
+    ros::ServiceServer m_pcTrajectoryService;
+    /** This returns the list of locations of the robot from the latest reset. */
+    ros::ServiceServer m_pcDoneService;
 
     /**
      * Calculate the fitness based on a distance to the object.
@@ -48,6 +83,20 @@ protected:
     Real m_fScore;
 
     Real m_fMaxDistance;
+
+    SInitSetup m_vecResetLocation;
+
+    CFootBotEntity* m_pcFootBot;
+
+    CRandom::CRNG* m_pcRNG;
+
+    bool m_bDone;
+
+private:
+
+    Real m_fFitnessPower;
+
+    std::vector<geometry_msgs::Point> m_vLocations;
 };
 
 #endif
