c52edbe4bf4dc82565a300f4f7a9f9c1ebd5e90d
matthijsdentoom <m.dentoom@student.tudelft.nl>
2019-07-24 11:17:53 +0200

    Added random orientation to come and go experiment

--- plugin/argos_loop_functions/come_and_go_loop_function.cpp
+++ plugin/argos_loop_functions/come_and_go_loop_function.cpp
@@ -6,6 +6,7 @@
 #include "fitness_functions/come_and_go_fitness_function.h"
 
 #include <argos3/core/utility/math/vector3.h>
+#include <argos3/core/utility/math/angles.h>
 
 void CComeAndGoLoopFunction::Init(TConfigurationNode &t_node)
 {
@@ -26,8 +27,11 @@ void CComeAndGoLoopFunction::Reset()
     CVector3 new_position(m_vecResetLocation.Position[0] + x_offset, m_vecResetLocation.Position[1] + y_offset,
             m_vecResetLocation.Position[0]);
 
-    // TODO: randomize orientation.
-    if(!MoveEntity(m_pcFootBot->GetEmbodiedEntity(), new_position, m_vecResetLocation.Orientation, false ))
+    // Randomize the heading.
+    CRadians random_heading = m_pcRNG->Uniform(CRange<CRadians>(-CRadians::PI_OVER_TWO, CRadians::PI_OVER_TWO));
+    m_vecResetLocation.Orientation.FromEulerAngles(random_heading, CRadians::ZERO, CRadians::ZERO);
+
+    if(!MoveEntity(m_pcFootBot->GetEmbodiedEntity(), new_position, m_vecResetLocation.Orientation, false))
     {
         LOGERR << "Can't move robot in <" << new_position
                << ">, <" << m_vecResetLocation.Orientation << ">" << std::endl;
