35fba5dffa2fe6df799bf9ea81a7558a31415e98
matthijsdentoom <m.dentoom@student.tudelft.nl>
2019-08-07 11:42:42 +0200

    Added unfiltered state gatherer.

--- scripts/state_gatherer.py
+++ scripts/state_gatherer.py
@@ -55,3 +55,38 @@ class StatesGatherer:
 
             for states in itervalues(self.received_state_sets):
                 writer.writerow(states)
+
+
+class UnfilteredStatesGatherer:
+    """ This class receives all sim_reports and writes them to a file, no filtering is applied."""
+
+    def __init__(self, name_space, base_dir):
+        stripped_ns = name_space.replace('/', "")
+        self.states_file = base_dir + "unfiltered_sim_report_{0}.csv".format(stripped_ns)
+        rospy.Subscriber(name_space + 'simreport_topic', SimulationReport, self.callback)
+
+        create_file(self.states_file)
+        self.received_state_sets = {}
+        self.current_generation = 0
+
+    def callback(self, data):
+        assert len(data.state_sequence) == len(data.locations) or len(data.state_sequence) == 0
+
+        state_dict = {}
+        for state in data.state_sequence:
+            if state not in state_dict:
+                state_dict[state] = 0
+
+            state_dict[state] += 1
+
+        state_usage = []
+        for key, value in iteritems(state_dict):
+            state_usage.append(key)
+            state_usage.append(value)
+
+        data_array = [data.header.generation, data.header.key, data.score, data.at_light] + state_usage
+
+        # Write the data to the data_file.
+        with open(self.states_file, 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(data_array)
